66b0c11cb29455a28f7c248dd23b2068ab1a04a5
Randil Hasanga <randil_2020690@fot.ruh.ac.lk>
2025-05-03 23:56:47 +0530

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,6 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @UseGuards(AuthenticatedGuard)
   getHello(): string {
     return this.appService.getHello();
   }
