c76437d73ef3429125234102382bf1691c90ef8b
Randil Hasanga <randil_2020690@fot.ruh.ac.lk>
2025-05-03 23:26:22 +0530

    bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -13,22 +13,6 @@ export class AuthService {
         private readonly userModel: Model<UserDocument>,
     ) { }
 
-    // async validateUser(userDetails: UserDetails) {
-    //     try {
-    //         const user = await this.userModel.findOne({ email: userDetails.email });
-    //         console.log("User found", user);
-    //         if (user) return user;
-    //         console.log("User not found, creating new user");
-    //         const newUser = await this.userModel.create(userDetails);
-    //         console.log('Auth service', newUser);
-    //         return newUser;
-
-    //     } catch (error) {
-    //         console.error("Error validating user", error);
-    //         throw new Error('Error validating user');
-    //     }
-    // }
-
     async validateUser(userDetails: UserDetails) {
         try {
             const user = await this.userModel.findOne({ email: userDetails.email });
