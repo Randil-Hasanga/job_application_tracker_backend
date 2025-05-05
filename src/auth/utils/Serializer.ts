e414c03bcea915a40c1c418db68a0a02541ac8a9
Randil Hasanga <randil_2020690@fot.ruh.ac.lk>
2025-05-05 17:03:24 +0530

    bug check

--- src/auth/utils/Serializer.ts
+++ src/auth/utils/Serializer.ts
@@ -1,3 +1,37 @@
+// import { Inject, Injectable } from "@nestjs/common";
+// import { PassportSerializer } from "@nestjs/passport";
+// import { AuthService } from "../auth.service";
+// import { User, UserDocument } from "../entities/user.entity";
+
+// @Injectable()
+// export class SessionSerializer extends PassportSerializer {
+//   constructor(@Inject('AUTH_SERVICE') private readonly authService: AuthService) {
+//     super();
+//   }
+
+//   serializeUser(user: UserDocument, done: Function) {
+//     console.log("Inside Serialiser : Serializing USER >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+//     done(null, { _id: user._id, email: user.email, picture: user.picture, displayName: user.displayName });
+//   }
+
+//   async deserializeUser(payload: any, done: (err: any, user: any) => void) {
+//     try {
+//       console.log("Inside Deserializer : Deserializing USER >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+//       console.log('Deserializing user', payload);
+//       const user = await this.authService.findUser(payload._id);
+//       if (!user) {
+//         console.error('User not found during deserialization');
+//         return done(null, null); // No user found, log out
+//       }
+//       console.log('User found during deserialization', user);
+//       return done(null, user);
+//     } catch (err) {
+//       console.error('Error during deserialization', err);
+//       return done(err, null); // In case of unexpected errors
+//     }
+//   }
+// }
+
 import { Inject, Injectable } from "@nestjs/common";
 import { PassportSerializer } from "@nestjs/passport";
 import { AuthService } from "../auth.service";
@@ -9,21 +43,21 @@ export class SessionSerializer extends PassportSerializer {
     super();
   }
 
-  serializeUser(user: UserDocument, done: Function) {
-    console.log("Inside Serialiser : Serializing USER >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+  serializeUser (user: UserDocument, done: Function) {
+    console.log("Inside Serializer: Serializing USER >>>>>>>>>>>>>>>>>>>>>>>>>>");
     done(null, { _id: user._id, email: user.email, picture: user.picture, displayName: user.displayName });
   }
 
-  async deserializeUser(payload: any, done: (err: any, user: any) => void) {
+  async deserializeUser (payload: any, done: (err: any, user: any) => void) {
     try {
-      console.log("Inside Deserializer : Deserializing USER >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+      console.log("Inside Deserializer: Deserializing USER >>>>>>>>>>>>>>>>>>>>>>>>>>");
       console.log('Deserializing user', payload);
-      const user = await this.authService.findUser(payload._id);
+      const user = await this.authService.findUser (payload._id);
       if (!user) {
-        console.error('User not found during deserialization');
+        console.error('User  not found during deserialization');
         return done(null, null); // No user found, log out
       }
-      console.log('User found during deserialization', user);
+      console.log('User  found during deserialization', user);
       return done(null, user);
     } catch (err) {
       console.error('Error during deserialization', err);
--- src/types/user.type.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    _id: string;
-    displayName: string;
-    email: string;
-    picture: string;
-    createdAt: string; // or Date, depending on usage
-    updatedAt: string; // or Date
-  }
-  
