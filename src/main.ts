e4461ef2730f66a3b6bce23688fb9ae3c4c6e05b
Randil Hasanga <randil_2020690@fot.ruh.ac.lk>
2025-05-24 09:47:38 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as session from 'express-session';
+import session from 'express-session';
 import * as passport from 'passport';
 import { config } from 'dotenv';
 import * as mongoose from 'mongoose';
